6cb6eaa17f450acec03e703c8c7232013e10ad0f
Brian Cunnie <brian.cunnie@gmail.com>
2016-09-02 08:03:44 -0700

    Check that `whois` matches `dig`

--- ci/tasks/check-dns.rb
+++ ci/tasks/check-dns.rb
@@ -20,4 +20,16 @@ check_domain(domain)
 puts "[PASS] #{domain} basic check ('DOMAIN' variable set & not empty)"
 
 whois_nameservers = get_whois_nameservers(domain)
-puts "[PASS] #{domain} has whois entry with nameservers"
+#whois_nameservers = [ 'ns-aws.nono.com', 'ns-he.nono.com' ] # testing
+puts "[PASS] #{domain} has whois entry with nameservers #{whois_nameservers.join(", ")}"
+
+whois_nameservers.each do |whois_nameserver|
+  dig = `dig +short ns sslip.io #{whois_nameserver}`
+	dig_nameservers = dig.split(/\n+/)
+  if ( whois_nameservers.sort == dig_nameservers.sort )
+    puts "[PASS] #{whois_nameserver}'s NS records match whois"
+  else
+    puts "[FAIL] #{whois_nameserver}'s NS records do NOT match whois: #{dig_nameservers.join(", ")}"
+  end
+  #p "#{whois_nameserver}: #{nameservers}"
+end
