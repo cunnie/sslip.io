216aaa7d703127610c001867aff2c7cc76735e26
Brian Cunnie <bcunnie@vmware.com>
2020-08-17 08:51:17 -0700

    `NameToA()` has better coverage
    I couldn't resist—every network I used in my examples is special.

--- src/xip/xip_test.go
+++ src/xip/xip_test.go
@@ -19,15 +19,15 @@ var _ = Describe("Xip", func() {
 			},
 			// dots
 			Entry("loopback", "127.0.0.1", dnsmessage.AResource{A: [4]byte{127, 0, 0, 1}}),
-			Entry("loopback with domain", "127.0.0.1.com", dnsmessage.AResource{A: [4]byte{127, 0, 0, 1}}),
-			Entry("loopback with domain and www", "www.127.0.0.1.com", dnsmessage.AResource{A: [4]byte{127, 0, 0, 1}}),
-			Entry("pre and post", "nono.io.10.0.9.30.sslip.io", dnsmessage.AResource{A: [4]byte{10, 0, 9, 30}}),
-			Entry("pre and post", "nono.io.10.0.9.30.sslip.io", dnsmessage.AResource{A: [4]byte{10, 0, 9, 30}}),
-			Entry("two IPs, grabs the leftmost", "nono.io.10.0.9.30.172.16.0.30.sslip.io", dnsmessage.AResource{A: [4]byte{10, 0, 9, 30}}),
+			Entry("255 with domain", "255.254.253.252.com", dnsmessage.AResource{A: [4]byte{255, 254, 253, 252}}),
+			Entry(`"This" network, pre-and-post`, "nono.io.0.1.2.3.sslip.io", dnsmessage.AResource{A: [4]byte{0, 1, 2, 3}}),
+			Entry("private network, two IPs, grabs the leftmost", "nono.io.172.16.0.30.172.31.255.255.sslip.io", dnsmessage.AResource{A: [4]byte{172, 16, 0, 30}}),
 			// dashes
-			Entry("loopback", "127-0-0-1", dnsmessage.AResource{A: [4]byte{127, 0, 0, 1}}),
-			Entry("loopback with domain", "127-0-0-1-com", dnsmessage.AResource{A: [4]byte{127, 0, 0, 1}}),
-			Entry("loopback with domain and www", "www-127-0-0-1-com", dnsmessage.AResource{A: [4]byte{127, 0, 0, 1}}),
+			Entry("shared address with dashes", "100-64-1-2", dnsmessage.AResource{A: [4]byte{100, 64, 1, 2}}),
+			Entry("link-local with domain", "169-254-168-253-com", dnsmessage.AResource{A: [4]byte{169, 254, 168, 253}}),
+			Entry("IETF protocol assignments with domain and www", "www-192-0-0-1-com", dnsmessage.AResource{A: [4]byte{192, 0, 0, 1}}),
+			// dots-and-dashes, mix-and-matches
+			Entry("test-net address with dots-and-dashes", "www-192.0-2.3.example-me.com", dnsmessage.AResource{A: [4]byte{192, 0, 2, 3}}),
 		)
 		DescribeTable("when it does not match an IP address",
 			func(fqdn string) {
@@ -41,7 +41,7 @@ var _ = Describe("Xip", func() {
 			Entry("canonical domain", "sslip.io"),
 			Entry("www", "www.sslip.io"),
 			Entry("a lone number", "538.sslip.io"),
-			Entry("too big", "256.255.255.255"),
+			Entry("too big", "256.254.253.252"),
 		)
 	})
 })
