65bb857041ed0f9c255ed18c99b3608ce2df110c
Brian Cunnie <bcunnie@vmware.com>
2020-08-16 19:02:02 -0700

    `NameToA()` accommodates hyphens ("-") in hostnames

--- src/xip/xip.go
+++ src/xip/xip.go
@@ -5,10 +5,11 @@ import (
 	"golang.org/x/net/dns/dnsmessage"
 	"net"
 	"regexp"
+	"strings"
 )
 
 // https://stackoverflow.com/questions/53497/regular-expression-that-matches-valid-ipv6-addresses
-var ipv4RE= regexp.MustCompile(`(^|\.)(((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))($|\.)`)
+var ipv4RE= regexp.MustCompile(`(^|[.-])(((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])[.-]){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))($|[.-])`)
 
 func NameToA (fqdnString string) (dnsmessage.AResource, error) {
 	fqdn:=[]byte(fqdnString)
@@ -17,6 +18,7 @@ func NameToA (fqdnString string) (dnsmessage.AResource, error) {
 	}
 
 	match := string(ipv4RE.FindSubmatch(fqdn)[2])
+	match = strings.Replace(match, "-", ".", -1)
 	ipv4address := net.ParseIP(match).To4()
 
 	return dnsmessage.AResource{A: [4]byte{ipv4address[0], ipv4address[1], ipv4address[2], ipv4address[3]}}, nil
