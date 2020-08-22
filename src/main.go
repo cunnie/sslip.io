2bd50a22414e8c6d2419b54ef5dc7e86aed71f91
Brian Cunnie <bcunnie@vmware.com>
2020-08-22 16:13:56 -0700

    sslip.io: lean `main.go`
    Moved much of the processing of DNS messages into the library. Testing a
    library is easier than testing `main`, so I like to keep a lean
    `main`.

--- src/main.go
+++ src/main.go
@@ -1,12 +1,10 @@
 package main
 
 import (
-	"encoding/json"
 	"github.com/cunnie/sslip.io/src/xip"
 	"log"
+	"net"
 )
-import "net"
-import "golang.org/x/net/dns/dnsmessage"
 
 func main() {
 	conn, err := net.ListenUDP("udp", &net.UDPAddr{Port: 53})
@@ -14,29 +12,20 @@ func main() {
 		log.Fatal(err.Error())
 	}
 
-	var m dnsmessage.Message
-	buf := make([]byte, 512)
+	query := make([]byte, 512)
 
 	for {
-		_, _, err := conn.ReadFromUDP(buf)
+		_, addr, err := conn.ReadFromUDP(query)
 		if err != nil {
 			log.Println(err.Error())
 			break
 		}
-		err = m.Unpack(buf)
+
+		response, err := xip.QueryResponse(query)
 		if err != nil {
 			log.Println(err.Error())
 			break
 		}
-		for _, question := range m.Questions {
-			jsonQuestion, err := json.Marshal(question)
-			if err != nil {
-				log.Println(err.Error())
-			}
-			answer, _ := xip.NameToA(question.GoString())
-			jsonAnswer, err := json.Marshal(answer)
-			log.Println(jsonQuestion)
-			log.Println(jsonAnswer)
-		}
+		_, err = conn.WriteToUDP(response, addr)
 	}
 }
