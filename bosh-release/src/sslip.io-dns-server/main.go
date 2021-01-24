115973bc1232f0c1e592108b16b5ed4b8e18863d
Brian Cunnie <brian.cunnie@gmail.com>
2021-01-23 21:17:34 -0800

    Binding to port 53: better messaging
    The bulk of this commit is to address problems under Linux:
    
    - The user needs to be root to bind to port 53 (or have the
    `CAP_NET_BIND_SERVICE` capability), so if we have a permissions-problem,
    we say, "try `sudo`".
    
    - If we can't bind to `INADDR_ANY`, and we probably can't because
    "`systemd-resolved` provides a local DNS stub listener on IP address
    127.0.0.53" on port 53, which prevents us from binding, we fallback
    to attempting to bind to every available address individually.
    
    This commit bloats `main.go`, which I have mixed feelings about because
    it's untested.

--- bosh-release/src/sslip.io-dns-server/main.go
+++ bosh-release/src/sslip.io-dns-server/main.go
@@ -1,27 +1,67 @@
 package main
 
 import (
+	"errors"
 	"log"
 	"net"
 	"os"
-	"errors"
+	"runtime"
+	"strings"
+	"sync"
+	"syscall"
 
 	"xip/xip"
 )
 
 func main() {
+	var wg sync.WaitGroup
 	conn, err := net.ListenUDP("udp", &net.UDPAddr{Port: 53})
-	if err != nil {
-		//  err is usually a net.OpError wrapping an os.SyscallError
-		var e *os.SyscallError
-		if errors.As(err, &e) {
-			if os.IsPermission(e) {
-				log.Println("Invoke me with `sudo` because I don't have permission to bind to port 53.")
+	//  common err hierarchy: net.OpError → os.SyscallError → syscall.Errno
+	switch {
+	case err == nil:
+		log.Println(`Successfully bound to all interfaces, port 53.`)
+		wg.Add(1)
+		readFrom(conn, &wg)
+	case isErrorPermissionsError(err):
+		log.Println("Try invoking me with `sudo` because I don't have permission to bind to port 53.")
+		log.Fatal(err.Error())
+	case isErrorAddressAlreadyInUse(err):
+		log.Println(`I couldn't bind to "0.0.0.0:53" (INADDR_ANY, all interfaces), so I'll try to bind to each address individually.`)
+		ipCIDRs := listLocalIPCIDRs()
+		var boundIPsPorts, unboundIPs []string
+		for _, ipCIDR := range ipCIDRs {
+			ip, _, err := net.ParseCIDR(ipCIDR)
+			if err != nil {
+				log.Printf(`I couldn't parse the local interface "%s".`, ipCIDR)
+				continue
+			}
+			conn, err = net.ListenUDP("udp", &net.UDPAddr{
+				IP:   ip,
+				Port: 53,
+				Zone: "",
+			})
+			if err != nil {
+				unboundIPs = append(unboundIPs, ip.String())
+			} else {
+				wg.Add(1)
+				boundIPsPorts = append(boundIPsPorts, conn.LocalAddr().String())
+				go readFrom(conn, &wg)
 			}
 		}
+		if len(boundIPsPorts) > 0 {
+			log.Printf(`I bound to the following: "%s"`, strings.Join(boundIPsPorts, `", "`))
+		}
+		if len(unboundIPs) > 0 {
+			log.Printf(`I couldn't bind to the following IPs: "%s"`, strings.Join(unboundIPs, `", "`))
+		}
+	default:
 		log.Fatal(err.Error())
 	}
+	wg.Wait()
+}
 
+func readFrom(conn *net.UDPConn, wg *sync.WaitGroup) {
+	defer wg.Done()
 	for {
 		query := make([]byte, 512)
 		_, addr, err := conn.ReadFromUDP(query)
@@ -40,4 +80,54 @@ func main() {
 			log.Printf("%v.%d %s", addr.IP, addr.Port, logMessage)
 		}()
 	}
+}
+
+func listLocalIPCIDRs() []string {
+	var ifaces []net.Interface
+	var cidrStrings []string
+	var err error
+	if ifaces, err = net.Interfaces(); err != nil {
+		panic(err)
+	}
+	for _, iface := range ifaces {
+		var cidrs []net.Addr
+		if cidrs, err = iface.Addrs(); err != nil {
+			panic(err)
+		}
+		for _, cidr := range cidrs {
+			cidrStrings = append(cidrStrings, cidr.String())
+		}
+	}
+	return cidrStrings
+}
+
+// Thanks https://stackoverflow.com/a/52152912/2510873
+func isErrorAddressAlreadyInUse(err error) bool {
+	var eOsSyscall *os.SyscallError
+	if !errors.As(err, &eOsSyscall) {
+		return false
+	}
+	// errors.As(err, *syscall.Errno) doesn't work, so we fall back to old method
+	errErrno, ok := eOsSyscall.Err.(syscall.Errno)
+	if !ok {
+		return false
+	}
+	if errErrno == syscall.EADDRINUSE {
+		return true
+	}
+	const WSAEADDRINUSE = 10048
+	if runtime.GOOS == "windows" && errErrno == WSAEADDRINUSE {
+		return true
+	}
+	return false
+}
+
+func isErrorPermissionsError(err error) bool {
+	var eOsSyscall *os.SyscallError
+	if errors.As(err, &eOsSyscall) {
+		if os.IsPermission(eOsSyscall) {
+			return true
+		}
+	}
+	return false
 }
