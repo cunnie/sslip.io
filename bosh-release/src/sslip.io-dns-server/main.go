4ed88614569f0296e2ee3f14f772cafec2cf6917
Brian Cunnie <brian.cunnie@gmail.com>
2022-03-03 08:09:15 -0800

    Download blocklist hourlyhour, not hourly per boundIP
    Previously we would download the blocklist every hour for every address
    we've bound to, which, on Linux machines, could easily amount to 8
    addresses (loopback, IPv4, several IPv6). Linux, if you recall, has a
    systemd nameserver bound to 127.0.0.53, forcing us to bind to each
    address individually. Downloading multiple identical versions of the
    blocklist was inefficient.
    
    With this commit, it downloads the blocklist only once per hour,
    regardless of the number of individual IP addresses listened on.
    
    But what really excites me about this commit is that I've moved much of
    the initialization of the `xip.Xip` struct out of `main()` and into
    `xip.NewXip()`. This makes `main()` lean again, and `xip.Xip` has gotten
    complex enough that it warrants its own constructor.

--- bosh-release/src/sslip.io-dns-server/main.go
+++ bosh-release/src/sslip.io-dns-server/main.go
@@ -1,42 +1,29 @@
 package main
 
 import (
-	"context"
 	"errors"
 	"flag"
-	"fmt"
 	"log"
 	"net"
-	"net/http"
 	"os"
 	"runtime"
 	"strings"
 	"sync"
 	"syscall"
-	"time"
 	"xip/xip"
-
-	clientv3 "go.etcd.io/etcd/client/v3"
 )
 
 func main() {
 	var wg sync.WaitGroup
-	var err error
-	var x = &xip.Xip{Metrics: xip.Metrics{Start: time.Now()}}
 	// the sole flag, `-etcdHost`, is primarily meant for integration tests
 	var etcdEndpoint = flag.String("etcdHost", "localhost:2379", "etcd")
 	var blocklistURL = flag.String("blocklistURL", "https://raw.githubusercontent.com/cunnie/sslip.io/main/etc/blocklist.txt", `a list of "forbidden" names`)
 	flag.Parse()
 
-	// connect to `etcd`; if there's an error, set etcdCli to `nil` and that to
-	// determine whether to use a local key-value store instead
-	x.Etcd, err = clientv3New(*etcdEndpoint)
-	if err != nil {
-		log.Println(fmt.Errorf("failed to connect to etcd at %s; using local key-value store instead: %w", *etcdEndpoint, err))
-	} else {
-		log.Printf("Successfully connected to etcd at %s\n", *etcdEndpoint)
+	x, logmessages := xip.NewXip(*etcdEndpoint, *blocklistURL)
+	for _, logmessage := range logmessages {
+		log.Println(logmessage)
 	}
-	// I don't need to `defer etcdCli.Close()` it's redundant in the main routine: when main() exits, everything is closed.
 
 	conn, err := net.ListenUDP("udp", &net.UDPAddr{Port: 53})
 	//  common err hierarchy: net.OpError → os.SyscallError → syscall.Errno
@@ -85,46 +72,6 @@ func main() {
 
 func readFrom(conn *net.UDPConn, wg *sync.WaitGroup, x *xip.Xip, blocklistURL string) {
 	defer wg.Done()
-	// We want to make sure that our DNS server isn't used in a DNS amplification attack.
-	// The endpoint we're worried about is metrics.status.sslip.io, whose reply is
-	// ~400 bytes with a query of ~100 bytes (4x amplification). We accomplish this by
-	// using channels with a quarter-second delay. Max throughput 1.2 kBytes/sec.
-	//
-	// We want to balance this delay against our desire to run tests quickly, so we buffer
-	// the channel with enough room to accommodate our tests.
-	//
-	// We realize that, if we're listening on several network interfaces, we're throttling
-	// _per interface_, not from a global standpoint, but we didn't want to clutter
-	// main() more than necessary.
-	//
-	// We also want to have fun playing with channels
-	dnsAmplificationAttackDelay := make(chan struct{}, xip.MetricsBufferSize)
-	x.DnsAmplificationAttackDelay = dnsAmplificationAttackDelay
-	go func() {
-		// fill up the channel's buffer so that our tests aren't slowed down (~85 tests)
-		for i := 0; i < xip.MetricsBufferSize; i++ {
-			dnsAmplificationAttackDelay <- struct{}{}
-		}
-		// now put on the brakes for users trying to leverage our server in a DNS amplification attack
-		for {
-			dnsAmplificationAttackDelay <- struct{}{}
-			time.Sleep(250 * time.Millisecond)
-		}
-	}()
-	go func() {
-		for {
-			blocklistStrings, blocklistCDIRs, err := readBlocklist(blocklistURL)
-			if err != nil {
-				log.Println(fmt.Errorf("couldn't get blocklist at %s: %w", blocklistURL, err))
-			} else {
-				log.Printf("Successfully downloaded blocklist from %s: %v, %v", blocklistURL, blocklistStrings, blocklistCDIRs)
-				x.BlocklistStrings = blocklistStrings
-				x.BlocklistCDIRs = blocklistCDIRs
-				x.BlocklistUpdated = time.Now()
-			}
-			time.Sleep(1 * time.Hour)
-		}
-	}()
 	for {
 		query := make([]byte, 512)
 		_, addr, err := conn.ReadFromUDP(query)
@@ -191,46 +138,4 @@ func isErrorPermissionsError(err error) bool {
 		}
 	}
 	return false
-}
-
-// clientv3New attempts to connect to local etcd and retrieve a key to make
-// sure the connection works. If for any reason it fails it returns nil +
-// error
-func clientv3New(etcdEndpoint string) (*clientv3.Client, error) {
-	etcdEndpoints := []string{etcdEndpoint}
-	etcdCli, err := clientv3.New(clientv3.Config{
-		Endpoints:   etcdEndpoints,
-		DialTimeout: 250 * time.Millisecond,
-	})
-	if err != nil {
-		return nil, err
-	}
-	// Let's do a query to determine if etcd is really, truly there
-	ctx, cancel := context.WithTimeout(context.Background(), time.Millisecond*500)
-	defer cancel()
-	_, err = etcdCli.Get(ctx, "some-silly-key, doesn't matter if it exists")
-	if err != nil {
-		return nil, err
-	}
-	return etcdCli, nil
-}
-
-// readBlocklist downloads the blocklist of domains & CIDRs that are forbidden
-// because they're used for phishing (e.g. "raiffeisen")
-func readBlocklist(blocklistURL string) (blocklistStrings []string, blocklistCIDRs []net.IPNet, err error) {
-	resp, err := http.Get(blocklistURL)
-	if err != nil {
-		log.Println(fmt.Errorf(`failed to download blocklist "%s": %w`, blocklistURL, err))
-	} else {
-		defer resp.Body.Close()
-		if resp.StatusCode > 299 {
-			log.Printf(`failed to download blocklist "%s", HTTP status: "%d"`, blocklistURL, resp.StatusCode)
-		} else {
-			blocklistStrings, blocklistCIDRs, err = xip.ReadBlocklist(resp.Body)
-			if err != nil {
-				log.Println(fmt.Errorf(`failed to parse blocklist "%s": %w`, blocklistURL, err))
-			}
-		}
-	}
-	return blocklistStrings, blocklistCIDRs, err
 }
