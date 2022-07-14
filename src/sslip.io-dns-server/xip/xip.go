314ce692f2a981c8d689e511ae25f27b4d28a108
Brian Cunnie <brian.cunnie@gmail.com>
2022-07-14 09:06:35 -0700

    Update SOA to Bastille Day (7/14)
    I love Bastille Day. And I love bumping the SOA right before a new
    release.

--- src/sslip.io-dns-server/xip/xip.go
+++ src/sslip.io-dns-server/xip/xip.go
@@ -832,7 +832,7 @@ func SOAResource(name dnsmessage.Name) dnsmessage.SOAResource {
 	return dnsmessage.SOAResource{
 		NS:     name,
 		MBox:   mbox,
-		Serial: 2022042500,
+		Serial: 2022071400,
 		// cribbed the Refresh/Retry/Expire from google.com.
 		// MinTTL was 300, but I dropped to 180 for faster
 		// key-value propagation
