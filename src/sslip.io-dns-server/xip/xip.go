8c8aacacd3aa6f11b72cbd124a72e3dc53e1a9c2
Brian Cunnie <brian.cunnie@gmail.com>
2022-11-26 13:19:49 -0800

    Bump serial 2022110900 → 2022112600
    ...as a prelude to cutting a new release.

--- src/sslip.io-dns-server/xip/xip.go
+++ src/sslip.io-dns-server/xip/xip.go
@@ -849,7 +849,7 @@ func SOAResource(name dnsmessage.Name) dnsmessage.SOAResource {
 	return dnsmessage.SOAResource{
 		NS:     name,
 		MBox:   mbox,
-		Serial: 2022110900,
+		Serial: 2022112600,
 		// cribbed the Refresh/Retry/Expire from google.com.
 		// MinTTL was 300, but I dropped to 180 for faster
 		// key-value propagation
