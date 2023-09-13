f215be1397ad23a22fac89762bd4882048a35bed
Brian Cunnie <brian.cunnie@gmail.com>
2023-09-13 14:49:00 +0200

    `tools.go`: `go generate` installs ginkgo
    Works in conjunction with `go mod tidy`.
    
    I probably did this wrong, but I don't care:
    
    <https://www.jvt.me/posts/2022/06/15/go-tools-dependency-management/>

---
+++ src/sslip.io-dns-server/tools.go
@@ -0,0 +1,6 @@
+//go:generate go install github.com/onsi/ginkgo/v2/ginkgo
+
+package main
+
+// This file imports packages that are used when running go generate, or used
+// during the development process but not otherwise depended on by built code.
