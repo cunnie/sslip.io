30d51c3014e05f2fcebc39bf5d62fb1f3efd2adc
Brian Cunnie <brian.cunnie@gmail.com>
2024-11-16 16:09:43 -0800

    Update SOA to 11/15
    Jon Penn's birthday; probably the most life-changing birthday party I've
    ever been to.

--- xip/xip.go
+++ xip/xip.go
@@ -890,7 +890,7 @@ func SOAResource(name dnsmessage.Name) dnsmessage.SOAResource {
 	return dnsmessage.SOAResource{
 		NS:     name,
 		MBox:   mbox,
-		Serial: 20241105,
+		Serial: 20241115,
 		// cribbed the Refresh/Retry/Expire from google.com.
 		// MinTTL was 300, but I dropped to 180 for faster
 		// key-value propagation
