93d7d7298a4eca8d97ba91d77b930aaddda2ad24
Brian Cunnie <brian.cunnie@gmail.com>
2025-07-22 20:48:24 -0700

    Bump serial 20250615 → 20250723
    Brendan's sixtieth. Happy birthday.

--- xip/xip.go
+++ xip/xip.go
@@ -967,7 +967,7 @@ func SOAResource(name dnsmessage.Name) dnsmessage.SOAResource {
 	return dnsmessage.SOAResource{
 		NS:     name,
 		MBox:   mbox,
-		Serial: 20250615,
+		Serial: 20250723,
 		// cribbed the Refresh/Retry/Expire from google.com.
 		// MinTTL was 300, but I dropped to 180 for faster
 		// key-value propagation
