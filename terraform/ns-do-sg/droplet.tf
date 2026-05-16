8dd4fc7c56ff96b3ddedeefd7bab60668bc2f380
Brian Cunnie <brian.cunnie@gmail.com>
2026-05-15 18:59:13 -0700

    Terraform: introduce ns-do-sg
    We want to use Terraform, actually OpenTofu, to make upgrading our
    servers easier. They were originally handbuilt, but I want to automate
    to allow me greater flexibility. Specifically, I'd like to either
    upgrade to Ubuntu 26.04 or switch to Fedora 44.
    
    This commit introduces the Terraform manifests for one of our servers,
    the one Digital Ocean, ns-do-sg.

---
+++ terraform/ns-do-sg/droplet.tf
@@ -0,0 +1,18 @@
+resource "digitalocean_droplet" "nameserver" {
+  name       = "ns-do-sg.sslip.io"
+  region     = "sgp1"
+  size       = "s-2vcpu-4gb"
+  image      = "175306584"
+  monitoring = true
+  ipv6       = true
+  vpc_uuid   = "6d136c2d-bfff-428b-bfa0-30e2927948f0"
+}
+
+resource "digitalocean_reserved_ip" "nameserver_ip" {
+  region = "sgp1"
+}
+
+resource "digitalocean_reserved_ip_assignment" "nameserver_assign" {
+  ip_address = digitalocean_reserved_ip.nameserver_ip.ip_address
+  droplet_id = digitalocean_droplet.nameserver.id
+}
---
+++ terraform/ns-do-sg/provider.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = "~> 2.0"
+    }
+  }
+}
+
+variable "do_token" {}
+
+provider "digitalocean" {
+  token = var.do_token
+}
