a26b935df7558721b3d17d15dccab1c2d64348b5
Brian Cunnie <brian.cunnie@gmail.com>
2026-07-04 17:19:08 -0700

    Bump SOA to 7/4
    Happy 250th, Uncle Sam

--- xip/xip.go
+++ xip/xip.go
@@ -983,7 +983,7 @@ func SOAResource(name dnsmessage.Name) dnsmessage.SOAResource {
 	return dnsmessage.SOAResource{
 		NS:     name,
 		MBox:   mbox,
-		Serial: 20260516,
+		Serial: 20260704,
 		// cribbed the Refresh/Retry/Expire from google.com.
 		// MinTTL was 300, but I dropped to 180 for faster
 		// key-value propagation
